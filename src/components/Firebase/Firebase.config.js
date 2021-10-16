7f8f8f27233d01d4b11b84697a551f3596ec67d0
PROHOR <ahprohor88@gmail.com>
2021-10-16 20:41:36 +0600

    first commit

---
+++ src/components/Firebase/Firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDxqh6fa6HAPvozu_byOWN8QCcoJEcrnOg",
+    authDomain: "ema-john-simple-bf9cf.firebaseapp.com",
+    projectId: "ema-john-simple-bf9cf",
+    storageBucket: "ema-john-simple-bf9cf.appspot.com",
+    messagingSenderId: "934957337933",
+    appId: "1:934957337933:web:dde33d5bb589e2e2563b04"
+  };
+
+  export default firebaseConfig;
